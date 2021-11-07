4a74c90b32e79acd22239c23efedd30c680dddc7
illusion <dhruvtiwari110058@gmail.com>
2021-11-07 15:29:16 +0530

    final

---
+++ run.py
@@ -0,0 +1,6 @@
+from server import app
+
+if __name__ == '__main__':
+    # For local server: uncomment next line, and comment the next to next line
+    # app.run(debug=True, host='0.0.0.0', port="8081")
+    app.run()
--- server.py
+++ server.py
@@ -23,12 +23,7 @@ def index():
     except:
         abort(400)
 
+
 @app.errorhandler(400)
 def error():
     return jsonify({"Error": "Could not download image, please send the correct url or try again later."})
-
-
-if __name__ == '__main__':
-    # For local server: uncomment next line, and comment the next to next line
-    # app.run(debug=True, host='0.0.0.0', port="8081")
-    app.run()
